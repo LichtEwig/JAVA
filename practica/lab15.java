f96c26949e8f82cd87e3b541bcc8322361302bc7
LichtEwig <lichtewig73@gmail.com>
2023-08-07 20:44:37 -0300

    leer vectores

--- practica/lab15.java
+++ practica/lab15.java
@@ -45,3 +45,4 @@ public class lab15 {
         }
     }
 }
+/*   Leer por teclado una serie de 10 números enteros. La aplicación debe indicarnos si los números están ordenados de forma creciente, decreciente, o si están desordenados.*/
